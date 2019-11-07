c4c481b41890d1813b13efe8d402a08b07ae46a0
Barani Htut <barani.t16@gmail.com>
2019-11-06 23:01:46 -0800

    Submit

---
+++ level-1/static/js/app.js
@@ -0,0 +1,7 @@
+var tableData = data;
+
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+ 
+buildTable(tableData);
---
+++ level-1/static/js/data.js
@@ -0,0 +1,8 @@
+var data = [
+  {
+    datetime: "1/1/2010",
+    city: "benton",
+    state: "ar",
+    country: "us",
+  }
+];
